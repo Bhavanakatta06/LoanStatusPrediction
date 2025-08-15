381a89258617a7c4fdf6b389ea47f2c9999980f1
Bhavanakatta06 <bhavanakatta2004@gmail.com>
2025-08-15 15:04:13 +0530

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,67 @@
+# from logging import debug
+# from flask import Flask, render_template, request 
+# import utils  
+# from utils import preprocessdata 
+
+# app = Flask(__name__) 
+
+# @app.route('/') 
+# def home(): 
+#     return render_template('index.html') 
+
+# @app.route('/predict/', methods=['GET', 'POST'])
+# def predict():
+#     Gender = request.form.get('Gender')
+#     Married = request.form.get('Married')
+#     Dependents = request.form.get('Dependents')
+#     Education = request.form.get('Education')
+#     Self_Employed = request.form.get('Self_Employed')
+#     ApplicantIncome = request.form.get('ApplicantIncome')
+#     LoanAmount = request.form.get('LoanAmount')
+#     Credit_History = request.form.get('Credit_History')
+#     Property_Area = request.form.get('Property_Area')
+
+#     prediction = utils.preprocessdata(
+#         Gender, Married, Dependents, Education, Self_Employed,
+#         ApplicantIncome, LoanAmount, Credit_History, Property_Area
+#     )
+
+#     return render_template('result.html', prediction=prediction)
+
+# if __name__ == '__main__': 
+#     app.run(debug=True) 
+
+from flask import Flask, render_template, request
+import os
+import utils
+from utils import preprocessdata
+
+template_dir = os.path.abspath('../frontend/templates')
+static_dir = os.path.abspath('../frontend/static')
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict/', methods=['GET', 'POST'])
+def predict():
+    Gender = request.form.get('Gender')
+    Married = request.form.get('Married')
+    Dependents = request.form.get('Dependents')
+    Education = request.form.get('Education')
+    Self_Employed = request.form.get('Self_Employed')
+    ApplicantIncome = request.form.get('ApplicantIncome')
+    LoanAmount = request.form.get('LoanAmount')
+    Credit_History = request.form.get('Credit_History')
+    Property_Area = request.form.get('Property_Area')
+
+    prediction = preprocessdata(
+        Gender, Married, Dependents, Education, Self_Employed,
+        ApplicantIncome, LoanAmount, Credit_History, Property_Area
+    )
+
+    return render_template('result.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
